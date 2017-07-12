3831e98b0ec34f567d6317066264e17ab5172296
Suryanarayana <suryanarayana.mangipudi@affimity.com>
2017-07-12 10:42:39 -0700

    adding dashboard changes

---
+++ projects/AdminUI/master/js/modules/directives/sparklinePieChart.js
@@ -0,0 +1,23 @@
+/**=========================================================
+* smangipudi
+ * Module: primarypanel.js
+*
+ =========================================================*/
+App.directive('sparklinePieChart',   function() {
+  'use strict';
+ 
+  return {
+    restrict: 'E',
+    scope:{
+      id: '@',
+	    title:   '@',
+      piedata: '@',
+      barlabel: '@',
+      bartotal: '@',
+  	},
+  	controller: function ($scope, colors) {
+  		 $scope.colorByName = colors.byName;
+  	},
+    templateUrl: 'app/templates/chart/sparkline-progress.html'
+  };
+});
