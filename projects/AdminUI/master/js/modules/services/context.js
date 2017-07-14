48d0a48b723f6bd8b7d5fdaa5a4c876e7d8b419b
Suryanarayana <suryanarayana.mangipudi@affimity.com>
2017-07-14 09:22:50 -0700

    minor cleanup of ui like removing the nav button an scrollable venue dropdown.

--- projects/AdminUI/master/js/modules/services/context.js
+++ projects/AdminUI/master/js/modules/services/context.js
@@ -4,34 +4,27 @@
  * https://medium.com/opinionated-angularjs/techniques-for-authentication-in-angularjs-applications-7bbf0346acec
  =========================================================*/
  
-App.service('ContextService',['$location', function($location) {
-			/*var urlPath = $location.absUrl();
-			var parts = urlPath.split("/");
-			var index = 0;
-			for (var i = 0; i < parts.length; i++) {
-			   if (parts[i] == "WebUI") {
-				   index = i-1;
-			   }
-			}*/
+App.service('ContextService',['$location',function($location) {
+			
 	//dev.api.venuelytics.com
-			'use strict';
-			this.serverName = 'dev.api.venuelytics.com';
-			this.contextName = '//dev.api.venuelytics.com/WebServices/rsapi';
-
-			this.userVenues = {
-	            selectedVenueNumber: 521,
-	            selectedVenueName :"Monte Carlo",
-	            listIsOpen : false,
-	            available: {
-	                    'Monte Carlo' : 521,
-	                    'Myth': 832,
-	                    'Test Venue' : 170568
-	            }
-        	};
-        	this.setVenue = function (venueName, venueNumber) {
-                this.userVenues.listIsOpen = ! $scope.userVenues.listIsOpen;
-                this.userVenues.selectedVenueNumber = venueNumber;
-                this.userVenues.selectedVenueName = venueName;
-            };
-			return this;
-		}]);
+	'use strict';
+	this.serverName = 'dev.api.venuelytics.com';
+	this.contextName = '//dev.api.venuelytics.com/WebServices/rsapi';
+	this.userVenues = {
+        selectedVenueNumber: 521,
+        selectedVenueName :"Monte Carlo",
+        listIsOpen : false,
+        available: {
+                'Monte Carlo' : 521,
+                'Myth': 832,
+                'Test Venue' : 170568
+        }
+	};
+			
+	this.setVenue = function (venueName, venueNumber) {
+        this.userVenues.listIsOpen = ! $scope.userVenues.listIsOpen;
+        this.userVenues.selectedVenueNumber = venueNumber;
+        this.userVenues.selectedVenueName = venueName;
+    };
+	return this;
+}]);
