2f19c6654134c0a91233e28af779df12be7f9c96
Suryanarayana <suryanarayana.mangipudi@affimity.com>
2017-08-12 12:49:27 -0700

    fixing the layout glitches

--- projects/WebUI/code/src/js/controllers/bottleServiceCtrl.js
+++ projects/WebUI/code/src/js/controllers/bottleServiceCtrl.js
@@ -14,7 +14,9 @@ app.controller('BottleServiceController', ['$log', '$scope', '$http', '$location
             self.bottleCount = 1;
             $scope.selectedVenueMap = {};
             self.bottleMinimum = [];
+
             self.init = function() {
+                $("div.form-group").add("style", "margin-left: auto");
                 var date = new Date();
                 var today = new Date(date.getFullYear(), date.getMonth(), date.getDate());
                 $( "#requestDate" ).datepicker({autoclose:true, todayHighlight: true});
