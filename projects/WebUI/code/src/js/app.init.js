00d743808cf3d6ef28a163660e941e65f9794dd0
Suryanarayana <suryanarayana.mangipudi@affimity.com>
2017-10-04 18:27:14 -0700

    fixing the canching issue

--- projects/WebUI/code/src/js/app.init.js
+++ projects/WebUI/code/src/js/app.init.js
@@ -335,7 +335,7 @@ app.config(['$routeProvider', '$httpProvider', '$locationProvider', '$sceDelegat
     var version = new Date().getTime();
     $translateProvider.useStaticFilesLoader({
         prefix : 'assets/i18n/',
-        suffix : '.json?v=' + version
+        suffix : '.json'
     });
     $translateProvider.preferredLanguage('en');
     $translateProvider.useLocalStorage();
