125c855c402c486c74603d06e60fafb93e14b317
Saravanakumar46 <saravanakumar1991@hotmail.com>
2017-10-11 21:01:24 +0530

    add two blogs

--- projects/WebUI/code/src/js/controllers/blogPostCtrl.js
+++ projects/WebUI/code/src/js/controllers/blogPostCtrl.js
@@ -3,18 +3,20 @@
  * @date 28-JULY-2017
  */
 "use strict";
-app.controller('BlogPostController', ['$log', '$scope', 'DataShare','$translate', '$routeParams','APP_ARRAYS', 
-    function ($log, $scope, DataShare, $translate, $routeParams, APP_ARRAYS) {
+app.controller('BlogPostController', ['$log', '$scope', 'DataShare','$translate', '$routeParams','APP_ARRAYS','$rootScope', 
+    function ($log, $scope, DataShare, $translate, $routeParams, APP_ARRAYS, $rootScope) {
 
     	$log.log('Inside Blog Post Controller.');
 
     	var self = $scope;
 	    self.blogPostUrl ='';
         self.init = function() {
-          self.postId = $routeParams.postId;	
-          self.blogPost = APP_ARRAYS.nightlife;
-          self.blogPostUrl = APP_ARRAYS.blogPosts[self.postId];
-          $log.info("Readmore blog post:", self.blogPostUrl);
+            $rootScope.blogTab = 'active';
+            $rootScope.homeTab = '';
+            self.postId = $routeParams.postId;	
+            self.blogPost = APP_ARRAYS.nightlife;
+            self.blogPostUrl = APP_ARRAYS.blogPosts[self.postId];
+            $log.info("Readmore blog post:", self.blogPostUrl);
         };
 
         self.init();
