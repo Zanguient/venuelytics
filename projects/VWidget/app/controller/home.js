cecfbc525f7ccdef0fd56a8f588bcf303457089c
Suryanarayana <suryanarayana.mangipudi@affimity.com>
2017-10-16 22:05:37 -0700

    fixing the VWidget

--- projects/VWidget/app/controller/home.js
+++ projects/VWidget/app/controller/home.js
@@ -1,19 +1,16 @@
 'use strict';
 app.controller('homeController', ['$scope', function($scope) {
 
-  $scope.images = [
-    {
-      "logo":"images/logo.png",
-      "title":"venuelytics"
-    }
-  ];
+  $scope.logo = "images/logo.png";
+  $scope.title ="@venuelytics";
+    
   $scope.repeatName = [
     {
       "name":"Venuelytics Home",
       "Urls":"http://www.venuelytics.com/home"
     },
     {
-      "name":"venuelytics Business",
+      "name":"Venuelytics Business",
       "Urls":"http://www.venuelytics.com/searchBusiness"
     },
     {
@@ -21,34 +18,13 @@ app.controller('homeController', ['$scope', function($scope) {
       "Urls":"http://www.venuelytics.com/cities"
     },
     {
-      "name":"Venuelytics about",
+      "name":"Venuelytics About",
       "Urls":"http://www.venuelytics.com/about"
     },
     {
       "name":"Venuelytics Contact",
       "Urls":"http://www.venuelytics.com/contact"
-    },
-    {
-      "name":"Venuelytics blog",
-      "Urls":"http://www.venuelytics.com/blog"
-    },
-    {
-      "name":"Monte Carlo Venue ",
-      "Urls":"http://www.venuelytics.com/cities/Mountain%20View/521/VIP"
-    },
-    {
-      "name":"Pure Lounge Venue",
-      "Urls":"http://www.venuelytics.com/cities/Mountain%20View/548/VIP"
-    },
-    {
-      "name":"Test Lounge Venue",
-      "Urls":"http://www.venuelytics.com/cities/Mountain%20View/170597/VIP"
     }
   ];
-
-  $scope.avatar_style = {
-    'border-radius': '120px'
-  }
-
 
 }]);
