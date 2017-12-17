d3aa422084b564848949a024628f606408ffc84b
Suryanarayana Mangipudi <m_surya@yahoo.com>
2017-12-17 18:29:14 -0500

    Signed-off-by: Suryanarayana Mangipudi <m_surya@yahoo.com>

--- projects/AdminUI/master/js/modules/controllers/eventsCalendarController.js
+++ projects/AdminUI/master/js/modules/controllers/eventsCalendarController.js
@@ -33,8 +33,16 @@ App.controller('EventsCalendarController',  ['$state', '$stateParams','$scope', 
         }
         var obj = {};
         obj.title = event.eventType  + ':' +  event.eventName;  
-        var sDate = new Date(event.startDate);
-        var endDate = new Date(event.endDate);
+     
+        var startDate = event.startDate.substring(0,10);
+        var from = startDate.split("-");
+        var sDate = new Date(from[0], from[1] - 1, from[2]);
+
+        var endDate = event.endDate.substring(0,10);
+        from = endDate.split("-");
+        var endDate = new Date(from[0], from[1] - 1, from[2]);
+
+     
         var t = event.eventTime.split(":");
         var h = parseInt(t[0]);
         var m = parseInt(t[1]);
@@ -83,15 +91,15 @@ App.controller('EventsCalendarController',  ['$state', '$stateParams','$scope', 
           }
         }
       }
-      $('#calendar').fullCalendar( 'removeEvents');
-      $('#calendar').fullCalendar( 'addEventSource', $scope.calEvents );
-      $('#calendar').fullCalendar('rerenderEvents' );
+      $('#eventsCalendar').fullCalendar( 'removeEvents');
+      $('#eventsCalendar').fullCalendar( 'addEventSource', $scope.calEvents );
+      $('#eventsCalendar').fullCalendar('rerenderEvents' );
     });  
   };
 
 
   $scope.initCalendar = function () {
-    var calElement = $('#calendar');
+    var calElement = $('#eventsCalendar');
 	  	// check to remove elements from the list 
 	  calElement.fullCalendar({
       isRTL: $scope.app.layout.isRTL,
