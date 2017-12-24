de5adb1c83e2b6a33599ede904a7da27567c9144
Suryanarayana Mangipudi <m_surya@yahoo.com>
2017-12-23 23:39:19 -0500

    Signed-off-by: Suryanarayana Mangipudi <m_surya@yahoo.com>

--- projects/WebUI/code/src/js/controllers/homeCtrl.js
+++ projects/WebUI/code/src/js/controllers/homeCtrl.js
@@ -18,10 +18,21 @@ app.controller('HomeController', ['$log', '$scope', '$location', 'DataShare','$t
     $rootScope.selectedTab = 'home';
     $rootScope.videoUrl = APP_LINK.VIDEO_PLAY;
     
+    $scope.carouselData = [
+        {image: 'screen2.jpg', title: 'home.REAL_TIME_HOME', details: 'home.PERSONALIZED_SERVICES'},
+        {image: '2.jpg', title: 'home.REAL_TIME', details: 'home.RESERVATION_FOOD_DRINK'},
+        {image: '3.jpg', title: 'home.CREATE_NEW_SOURCES', details: 'home.WIFI_IN_VENUE_ORDERING'},
+        {image: 'screen4.jpg', title: 'home.VIP_PERSONALLY', details: 'home.CONSISTENTLY_REWARD'},
+        
+        {image: '5-1.jpg', title: 'home.IMPROVE_PRODUCTIVITY', details: 'home.FOR_CASINOS_CLUBS'},
+        {image: '6(1).jpg', title: 'home.REAL_TIME_MOBILE', details: 'home.IMMEDIATE_WIFI_ENABLED'},
+        {image: '7.jpg', title: 'home.CUSTOMER_SERVICE', title: 'home.EXPERIENCE'}
+
+
+    ];
     $scope.$on('$locationChangeStart', function(event) {
         var userSelectedTab = $location.absUrl().split('/').pop().split('?');
         $rootScope.canonicalURL = $location.absUrl();
-        self.navBar(userSelectedTab[0]);
     });
     self.init = function() {
 
@@ -60,10 +71,10 @@ app.controller('HomeController', ['$log', '$scope', '$location', 'DataShare','$t
         }
         $(document).ready(function () {
             
-            $('#carousel-example-generic').carousel({
+            $('#venuelytics-presentation').carousel({
                 interval: 3000
             });
-            $('#carousel-example-generic').carousel('cycle');
+            $('#venuelytics-presentation').carousel('cycle');
 
             var owl = $('.owl-carousel');
             owl.owlCarousel({
