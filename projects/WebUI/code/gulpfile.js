4cddc9a8f351b25d43243e204932bfa95b4fbb8c
Suryanarayana Mangipudi <m_surya@yahoo.com>
2018-01-13 19:14:17 -0800

    Signed-off-by: Suryanarayana Mangipudi <m_surya@yahoo.com>

--- projects/WebUI/code/gulpfile.js
+++ projects/WebUI/code/gulpfile.js
@@ -381,8 +381,8 @@ gulp.task('dist',['dist:pre'], function(cb) {
     config.compress = true;
     return gulp.src('dist/index.html')
      .pipe(cachebust.references())
-   //  .pipe(gulpif(config.compress, htmlmin({collapseWhitespace: true})))
-   //  .pipe(gulpif(config.compress, minifyInline()))
+     .pipe(gulpif(config.compress, htmlmin({collapseWhitespace: true})))
+     //.pipe(gulpif(config.compress, minifyInline()))
      .pipe(gulp.dest(config.folders.dist));
 });
 
