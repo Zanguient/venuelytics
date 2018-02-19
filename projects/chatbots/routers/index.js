449dcce7115ab4cbe6a2e919d446435afcee2a02
Suryanarayana Mangipudi <m_surya@yahoo.com>
2018-02-18 18:09:28 -0800

    fixing  configuration

--- projects/chatbots/routers/index.js
+++ projects/chatbots/routers/index.js
@@ -1,11 +1,11 @@
-'use strict'
+'use strict';
 var express = require("express");
 var router = express.Router();
 
 var facebookHandler = require('./facebook');
 var smsHandler = require('./sms');
 
-router.use('/facebook', facebookHandler)
-router.use('/smsbot', smsHandler)
+router.use('/facebook', facebookHandler);
+router.use('/smsbot', smsHandler);
 
 module.exports = router;
