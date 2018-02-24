93b64c322d4097dcec76e3012a8437f2a41f3493
Navaneethan C <navaneethan@10decoders.in>
2018-02-24 19:15:57 +0530

    reservation tab changes

--- projects/AdminUI/master/js/modules/controllers/venuemaps.js
+++ projects/AdminUI/master/js/modules/controllers/venuemaps.js
@@ -5,75 +5,83 @@
  * =========================================================
  */
 
-App.controller('VenueMapsController', ['$scope', '$state','$compile','$timeout', 'RestServiceFactory','DataTableService', 'toaster', '$stateParams','ngDialog',
-                                   function( $scope, $state, $compile, $timeout, RestServiceFactory, DataTableService, toaster, $stateParams, ngDialog) {
-  'use strict';
-  
-  $timeout(function(){
+App.controller('VenueMapsController', ['$scope', '$state', '$compile', '$timeout', 'RestServiceFactory', 'DataTableService', 'toaster', '$stateParams', 'ngDialog', 'DialogService',
+    function ($scope, $state, $compile, $timeout, RestServiceFactory, DataTableService, toaster, $stateParams, ngDialog, DialogService) {
+        'use strict';
 
-    if ( ! $.fn.dataTable ) return;
-    var columnDefinitions = [
-        { sWidth: "10%", aTargets: [0] },
-        { sWidth: "30%", aTargets: [1] },
-        { sWidth: "20%", aTargets: [2] },
-        { sWidth: "10%", aTargets: [3] },
-        {
-	    	"targets": [4],
-	    	"orderable": false,
-	    	"createdCell": function (td, cellData, rowData, row, col) {
-	    		var actionHtml = '<button title="Edit User" class="btn btn-default btn-oval fa fa-edit" '+
-            'ng-click="editVenueMap('+cellData+')"></button>&nbsp;&nbsp;</button>&nbsp;&nbsp;'+
-            '<button class="btn btn-default btn-oval fa fa-trash" ng-click="deleteBottle(' +row +','+cellData+')"></button>';
-	    		
-	    		$(td).html(actionHtml);
-	    		$compile(td)($scope);
-	    	  }
-    }];
-    
-	  DataTableService.initDataTable('venue_map_table', columnDefinitions);
-   
-    var promise = RestServiceFactory.VenueMapService().getAll({id: $stateParams.id, type: 'ALL'});
-    promise.$promise.then(function(data) {
-  	 
-    	var table = $('#venue_map_table').DataTable();
-    	
-    	data.map(function(venueMap) {
-    		table.row.add([venueMap.section, venueMap.days, venueMap.type, venueMap.elements.length, venueMap.id]);
-    	});
-    	table.draw();
-    });
-    
-  });  
+        $timeout(function () {
 
-  $scope.editVenueMap = function(venueMapId) {
-    $state.go('app.editVenueMap', {venueNumber: $stateParams.id, id: venueMapId});
-  };
+            if (!$.fn.dataTable) return;
+            var columnDefinitions = [
+                { sWidth: "10%", aTargets: [0] },
+                { sWidth: "30%", aTargets: [1] },
+                { sWidth: "20%", aTargets: [2] },
+                { sWidth: "10%", aTargets: [3] },
+                {
+                    "targets": [4],
+                    "orderable": false,
+                    "createdCell": function (td, cellData, rowData, row, col) {
 
-  $scope.createBottle = function(venueMapId) {
-    $state.go('app.editVenueMap', {venueNumber: $stateParams.id, id: 'new'});
-  };
-  $scope.deleteVenueMap = function(rowId, mapId) {
-    
-  };
+                        var actionHtml = '<button title="Edit User" class="btn btn-default btn-oval fa fa-edit"></button>' +
+                            '&nbsp;&nbsp;<button title="Delete User" class="btn btn-default btn-oval fa fa-trash"></button>';
 
-  $scope.createNewVenueMap = function() {
-    $state.go('app.venueMapedit', {id: 'new'});
-  };
-    
-  $scope.deleteBottle = function(rowId, cellData){
-    ngDialog.openConfirm({
-      template: 'deleteBottleId',
-      className: 'ngdialog-theme-default'
-    }).then(function (value) {
-      var target = {id: $stateParams.id ,tableId:cellData};
-      RestServiceFactory.VenueMapService().delete(target,  function(success){
-        var table = $('#venue_map_table').dataTable();
-        table.fnDeleteRow(rowId);
-      },function(error){
-        if (typeof error.data !== 'undefined') { 
-          toaster.pop('error', "Server Error", error.data.developerMessage);
-        } 
-      });
-    });
-  };
-}]);
+                        $(td).html(actionHtml);
+                        $compile(td)($scope);
+                    }
+                }];
+
+            DataTableService.initDataTable('venue_map_table', columnDefinitions);
+
+            var promise = RestServiceFactory.VenueMapService().getAll({ id: $stateParams.id, type: 'ALL' });
+            promise.$promise.then(function (data) {
+
+                var table = $('#venue_map_table').DataTable();
+
+                data.map(function (venueMap) {
+                    table.row.add([venueMap.section, venueMap.days, venueMap.type, venueMap.elements.length, venueMap]);
+                });
+                table.draw();
+            });
+            var table = $('#venue_map_table').DataTable();
+
+            $('#venue_map_table').on('click', '.fa-trash', function () {
+                $scope.deleteBottle(this, table);
+            });
+
+            $('#venue_map_table').on('click', '.fa-edit', function () {
+                $scope.editVenueMap(this, table);
+            });
+        });
+
+        $scope.editVenueMap = function (button, table) {
+            var targetRow = $(button).closest("tr");
+            var rowData = table.row(targetRow).data();
+            $state.go('app.editVenueMap', { venueNumber: $stateParams.id, id: rowData[4].id });
+        };
+
+        $scope.createBottle = function () {
+            $state.go('app.editVenueMap', { venueNumber: $stateParams.id, id: 'new' });
+        };
+        $scope.deleteVenueMap = function (rowId, mapId) {
+
+        };
+
+        $scope.createNewVenueMap = function () {
+            $state.go('app.venueMapedit', { id: 'new' });
+        };
+
+        $scope.deleteBottle = function (button, table) {
+            DialogService.confirmYesNo('Delete VenueMap?', 'Are you sure want to delete selected VenueMap?', function () {
+                var targetRow = $(button).closest("tr");
+                var rowData = table.row(targetRow).data();
+                var target = { id: $stateParams.id, tableId: rowData[4].id };
+                RestServiceFactory.VenueMapService().delete(target, function (data) {
+                    table.row(targetRow).remove().draw();
+                }, function (error) {
+                    if (typeof error.data !== 'undefined') {
+                        toaster.pop('error', "Server Error", error.data.developerMessage);
+                    }
+                });
+            });
+        };
+    }]);
