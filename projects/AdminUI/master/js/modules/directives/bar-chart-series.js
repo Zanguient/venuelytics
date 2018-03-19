133a62991760d7de9b1fc6bd10f4948bf985ea72
Saravanakumar46 <saravanakumar1991@hotmail.com>
2018-03-19 23:11:20 +0530

    Added tooltip for d3 and other changes.

--- projects/AdminUI/master/js/modules/directives/bar-chart-series.js
+++ projects/AdminUI/master/js/modules/directives/bar-chart-series.js
@@ -3,7 +3,7 @@
  * Module: stacked-bar-chart.js
 *
  =========================================================*/
-App.directive('seriesBarChart', function () {
+ App.directive('seriesBarChart', function () {
     'use strict';
 
     return {
@@ -38,7 +38,7 @@ App.directive('seriesBarChart', function () {
             }
         },
         controller: function ($scope) {
-            console.log("Location data-->:", $scope.url);
+            // console.log("Location data-->:", $scope.url);
             $scope.chart = new FlotChart($('#' + $scope.id), null);
             $scope.option = {
                 series: {
@@ -77,7 +77,7 @@ App.directive('seriesBarChart', function () {
             };
 
             $scope.drawChart = function (data) {
-                console.log("Options:", $scope.chartData);
+                // console.log("Options:", $scope.chartData);
                 $scope.option.xaxis.mode = $scope.mode;
                 if ($scope.mode === 'time') {
                     $scope.option.series.bars.lineWidth = 1;
@@ -142,12 +142,10 @@ App.directive('njBarChart', function () {
         link: function (scope, element, attrs) {
 
             scope.$watch('parameter', function (newValue, oldValue) {
-                console.log("Chart value:", newValue);
                 scope.drawD3Js(newValue);
             });
         }, controller: function ($scope) {
             $scope.drawD3Js = function (data) {
-                console.log("Inside d3")
                 var margin = { top: 20, right: 20, bottom: 30, left: 40 },
                     width = 960 - margin.left - margin.right,
                     height = 400 - margin.top - margin.bottom;
@@ -169,9 +167,14 @@ App.directive('njBarChart', function () {
                     .scale(y)
                     .orient("left");
 
+                d3.select("svg").remove();
+                
+                //Tooltip
+                var divTooltip = d3.select("body").append("div").attr("class", "toolTip");
+                
                 var color = d3.scale.ordinal()
                     .range(["#ca0020", "#f4a582", "#d5d5d5", "#92c5de", "#0571b0", "#FF8C00", "#6B486B"]);
-                d3.select("svg").remove();
+                
                 var svg = d3.select('#visitorsChartReasonD3').append("svg")
                     .classed("svg-container", true)
                     .attr("preserveAspectRatio", "xMinYMin meet")
@@ -182,8 +185,14 @@ App.directive('njBarChart', function () {
                     .append("g")
                     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+                    svg.append("rect")
+                    .attr("width", "86%")
+                    .attr("height", "88%")
+                    .attr("fill", "#FCFCFC");
+
+               
+
                 var fullJson = JSON.parse(data);
-                console.log("Json data:", angular.toJson(fullJson));
                 var response = fullJson[0];
                 var categorieNames = response.ticks.map(function (item) {
                     return item[1];
@@ -217,7 +226,6 @@ App.directive('njBarChart', function () {
                 x1.domain(rateNames).rangeRoundBands([0, x0.rangeBand()]);
                 y.domain([0, d3.max(dataValues, function (categorie) { return d3.max(categorie.values, function (d) { return d.value; }); })]);
 
-
                 svg.append("g")
                     .attr("class", "x axis")
                     .attr("transform", "translate(0," + height + ")")
@@ -243,36 +251,69 @@ App.directive('njBarChart', function () {
                     .style('font-weight', 'bold')
                     .text("");
 
-                svg.select('.y').transition().duration(500).delay(1300).style('opacity', '1');
-
-                var slice = svg.selectAll(".slice")
+                var bar = svg.selectAll(".bar")
                     .data(dataValues)
                     .enter().append("g")
-                    .attr("class", "g")
-                    .attr("transform", function (d) { return "translate(" + x0(d.categorie) + ",0)"; });
+                    .attr("class", "rect")
+                    .attr("transform", function(d) { return "translate(" + x0(d.categorie) + ",0)"; });
 
-                slice.selectAll("rect")
+                svg.select('.y').transition().duration(500).delay(1300).style('opacity', '1');
+
+                // var slice = svg.selectAll(".slice")
+                //     .data(dataValues)
+                //     .enter().append("g")
+                //     .attr("class", "g")
+                //     .attr("transform", function (d) { return "translate(" + x0(d.categorie) + ",0)"; });
+                
+                if(response.aggregateType === "Weekly" || response.aggregateType === "Monthly") {
+                    bar.selectAll("rect")
+                    .data(function (d) { return d.values; })
+                    .enter().append("rect")
+                    .attr("width", "20px")
+                    .attr("x", function (d) { return x1(d.rate); })
+                    .style("fill", function (d) { return color(d.rate) })
+                    .attr("y", function (d) { return y(0); })
+                    .attr("height", function (d) { return height - y(0); });
+                } else {
+                    bar.selectAll("rect")
                     .data(function (d) { return d.values; })
                     .enter().append("rect")
                     .attr("width", x1.rangeBand())
                     .attr("x", function (d) { return x1(d.rate); })
                     .style("fill", function (d) { return color(d.rate) })
                     .attr("y", function (d) { return y(0); })
-                    .attr("height", function (d) { return height - y(0); })
-                    .on("mouseover", function (d) {
-                        d3.select(this).style("fill", d3.rgb(color(d.rate)).darker(2));
-                    })
-                    .on("mouseout", function (d) {
-                        d3.select(this).style("fill", color(d.rate));
+                    .attr("height", function (d) { return height - y(0); });
+                }
+            
+
+                bar
+                    .on("mousemove", function(d){
+                        divTooltip.style("left", d3.event.pageX+10+"px");
+                        divTooltip.style("top", d3.event.pageY-25+"px");
+                        divTooltip.style("display", "inline-block");
+                        divTooltip.style("color", "black");
+                        divTooltip.style("z-index", "11");
+                        var x = d3.event.pageX, y = d3.event.pageY
+                        var elements = document.querySelectorAll(':hover');
+                        var l = elements.length
+                        l = l-1
+                        var elementData = elements[l].__data__
+                        divTooltip.html(elementData.rate+"<br>"+elementData.value+"%");
                     });
 
-                slice.selectAll("rect")
+                bar
+                    .on("mouseout", function(d){
+                        divTooltip.style("display", "none");
+                    });
+
+                bar.selectAll("rect")
                     .transition()
                     .delay(function (d) { return Math.random() * 1000; })
                     .duration(1000)
                     .attr("y", function (d) { return y(d.value); })
                     .attr("height", function (d) { return height - y(d.value); });
-
+                
+               
                 //Legend
                 var legend = svg.selectAll(".legend")
                     .data(dataValues[0].values.map(function (d) { return d.rate; }).reverse())
