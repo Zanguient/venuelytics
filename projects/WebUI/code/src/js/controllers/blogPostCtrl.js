b5ecfd6649ddd28e2bd0e208a554f8e6ce8f9226
sureshmuthu1991 <suresh.m@10decoders.in>
2018-05-21 13:05:41 +0530

    removed unwanted blogs code

--- projects/WebUI/code/src/js/controllers/blogPostCtrl.js
+++ projects/WebUI/code/src/js/controllers/blogPostCtrl.js
@@ -3,25 +3,29 @@
  * @date 28-JULY-2017
  */
 "use strict";
-app.controller('BlogPostController', ['$log', '$scope', 'DataShare','$translate', '$routeParams','APP_ARRAYS','$rootScope','ngMeta', 
+app.controller('BlogPostController', ['$log', '$scope', 'DataShare', '$translate', '$routeParams', 'APP_ARRAYS', '$rootScope', 'ngMeta',
     function ($log, $scope, DataShare, $translate, $routeParams, APP_ARRAYS, $rootScope, ngMeta) {
 
-    	$log.log('Inside Blog Post Controller.');
+        $log.log('Inside Blog Post Controller.');
 
-    	var self = $scope;
-	    self.blogPostUrl ='';
+        var self = $scope;
+        self.blogPostUrl = '';
         $rootScope.selectedTab = 'blogs';
-        self.init = function() {
+        self.init = function () {
             self.postId = $routeParams.postId;
             self.blogPost = APP_ARRAYS.nightlife;
-            self.blogPostUrl = APP_ARRAYS.blogPosts[self.postId];
-            self.blogPostImage = APP_ARRAYS.blogPostsImages[self.postId];
-            self.blogPostTitle = APP_ARRAYS.blogPostsTitle[self.postId];
-            self.blogPostDescription = APP_ARRAYS.blogPostDescription[self.postId];
-            $log.info("Readmore blog post:", self.blogPostUrl);
-            ngMeta.setTitle(self.blogPostTitle + " - Venuelytics");
-            ngMeta.setTag('image', self.blogPostImage);
-            ngMeta.setTag('description', self.blogPostDescription);
+            for (var i = 0; i < (APP_ARRAYS.blogs).length; i++) {
+                if (self.postId === APP_ARRAYS.blogs[i].link) {
+                    self.blogPostUrl = APP_ARRAYS.blogs[i].bindHtml;
+                    self.blogPostImage = APP_ARRAYS.blogs[i].image;
+                    self.blogPostTitle = APP_ARRAYS.blogs[i].title;
+                    self.blogPostDescription = APP_ARRAYS.blogs[i].description;
+                    $log.info("Readmore blog post:", self.blogPostUrl);
+                    ngMeta.setTitle(self.blogPostTitle + " - Venuelytics");
+                    ngMeta.setTag('image', self.blogPostImage);
+                    ngMeta.setTag('description', self.blogPostDescription);
+                }
+            }
         };
 
         self.init();
