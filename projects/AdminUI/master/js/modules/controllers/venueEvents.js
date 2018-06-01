a1c2985afd38930e238adbd43bf6effbf13d6820
sureshmuthu1991 <suresh.m@10decoders.in>
2018-06-01 12:33:55 +0530

    event list and calendar

--- projects/AdminUI/master/js/modules/controllers/venueEvents.js
+++ projects/AdminUI/master/js/modules/controllers/venueEvents.js
@@ -5,56 +5,58 @@
  * =========================================================
  */
 
-App.controller('VenueEventsController', ['$scope', '$state','$compile','$timeout', 'RestServiceFactory', '$stateParams','Session','ContextService', 'APP_EVENTS',
-                                   function($scope, $state, $compile, $timeout, RestServiceFactory, $stateParams, Session, contextService, APP_EVENTS) {
-  'use strict';
-  
-  $scope.events = [];
-  var target = {};
+App.controller('VenueEventsController', ['$scope', '$state', '$compile', '$timeout', 'RestServiceFactory', '$stateParams', 'Session', 'ContextService', 'APP_EVENTS',
+  function ($scope, $state, $compile, $timeout, RestServiceFactory, $stateParams, Session, contextService, APP_EVENTS) {
+    'use strict';
 
-  $scope.init = function() {
+    $scope.calendarClicked = function () {
+      $scope.content = 'app/views/venue/eventCalendar.html';
+    };
+    $scope.listClicked = function () {
+      $scope.content = 'app/views/venue-events/venue-event-list.html';
+    }
+    $scope.events = [];
+    var target = {};
+
+    $scope.init = function () {
       $scope.getEvents(contextService.userVenues.selectedVenueNumber);
-  };
-  
-  
-  $scope.getEvents = function(id) {
-    var target = {id: id};
-    var promise = RestServiceFactory.VenueService().getEvents(target);  
-      promise.$promise.then(function(data) {
-      $scope.events = data['venue-events'];
+    };
+
+    $scope.getEvents = function (id) {
+      var target = { id: id };
+      var promise = RestServiceFactory.VenueService().getEvents(target);
+      promise.$promise.then(function (data) {
+        $scope.events = data['venue-events'];
+      });
+    };
+
+    function formatDate(value) {
+      return value.getMonth() + 1 + "/" + value.getDate() + "/" + value.getYear();
+    }
+
+    $scope.enableCreateEvent = function () {
+      return Session.roleId >= 50;
+    };
+
+    $scope.createNewEvent = function () {
+      $state.go('app.editVenueEvent', { venueNumber: contextService.userVenues.selectedVenueNumber, id: 'new' });
+    };
+    $scope.$on(APP_EVENTS.venueSelectionChange, function (event, data) {
+      $scope.getEvents(contextService.userVenues.selectedVenueNumber);
     });
-  };
-
-  function formatDate(value) {
-    return value.getMonth()+1 + "/" + value.getDate() + "/" + value.getYear();
-  }
-  
-  $scope.enableCreateEvent = function() {
-    return Session.roleId >= 50;
-  };
-
-	$scope.createNewEvent = function() {
-		$state.go('app.editVenueEvent', {venueNumber: contextService.userVenues.selectedVenueNumber, id: 'new'});
-	};
-
-  $scope.$on(APP_EVENTS.venueSelectionChange, function(event, data) {
-    $scope.getEvents(contextService.userVenues.selectedVenueNumber);
-  });
-
-  $scope.$on(APP_EVENTS.deleteEvent, function(event, data) {
-    var index = -1;
-
-    for (var idx = 0 ; idx < $scope.events.length;  idx++) {
-      if ($scope.events[idx].id === data.event.id ) {
-        index = idx;
+    $scope.$on(APP_EVENTS.deleteEvent, function (event, data) {
+      var index = -1;
+      for (var idx = 0; idx < $scope.events.length; idx++) {
+        if ($scope.events[idx].id === data.event.id) {
+          index = idx;
+        }
       }
-    }
-    if (index > -1) {
-      $scope.events.splice(index,1);
-    }
+      if (index > -1) {
+        $scope.events.splice(index, 1);
+      }
 
-  });
+    });
 
-  $scope.init();
-  
-}]);
+    $scope.init();
+    
+  }]);
